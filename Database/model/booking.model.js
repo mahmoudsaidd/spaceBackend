23961423f79dd3cba9d7f7eabb20734e06e59fb9
mahmoudsaidd <mahmoudsaidd01@gmail.com>
2023-03-14 03:54:36 +0200

    Update booking.model.js

--- Database/model/booking.model.js
+++ Database/model/booking.model.js
@@ -5,7 +5,9 @@ const bookingSchema = new mongoose.Schema({
   
   room: { type: mongoose.Schema.Types.ObjectId, ref: 'room', required: true },
   duration: { type: Number, required: true }, //3ayzeen neshof data type aw function lel duration gher number de
-  time: { type: Date, required: true },
+  // time: { type: Date, required: true },
+  startTime: {type: Date, required: true},
+  endTime:{type: Date, required: true},
   fees: { type: Number },
   promoCode: { type: String },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
